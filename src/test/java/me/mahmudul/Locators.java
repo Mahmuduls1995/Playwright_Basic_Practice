69d940ede920afc40969943026ba59cb6198d21e
Sheikh Mahmudul <mahmuduls451@gmail.com>
2025-03-05 23:23:59 +0600

    lacateByTagName and Xpath

--- src/test/java/me/mahmudul/Locators.java
+++ src/test/java/me/mahmudul/Locators.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Locators {
     Playwright playwright;
     BrowserType browserType;
@@ -29,15 +31,43 @@ public class Locators {
     }
 
     @Test(priority = 1)
-    public void locators() throws InterruptedException {
+    public void locateById() throws InterruptedException {
         ElementHandle firstName = page.querySelector("#first-name"); // By ID
         firstName.fill("Mahmudul");
         Thread.sleep(2000);
+    }
 
+    @Test(priority = 2)
+    public void locateByName() throws InterruptedException {
         ElementHandle lastName = page.querySelector("[name='last-name']"); // By Name
         lastName.fill("Hasan");
         Thread.sleep(2000);
+    }
 
+    @Test(priority = 3)
+    public void locateByLinkText() throws InterruptedException {
+        ElementHandle link = page.querySelector("a:has-text('Back To Home')"); // By Link Text
+        link.click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 4)
+    public void locateByTagName() throws InterruptedException {
+        List<ElementHandle> element = page.querySelectorAll("input"); // By Name
+        System.out.println("Element Size: " + element.size());
+        Thread.sleep(3000);
+
+        for (ElementHandle handle : element) {
+            String id = handle.getAttribute("id");
+            System.out.println("ID: " + id);
+        }
+    }
+
+    @Test(priority = 5)
+    public void locateByXpath() throws InterruptedException {
+        ElementHandle element = page.querySelector("//input[@id='qualification']"); // By Xpath
+        element.fill("SQA");
+        Thread.sleep(3000);
     }
 
 
